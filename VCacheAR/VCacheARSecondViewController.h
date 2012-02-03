2d50dcea7d4efbb2e70ee5c573e3c5c3db12245e
Grif Priest <Grif@Grifs-Mac-mini.local>
2012-02-02 19:27:04 -0800

    not there yet but closer

--- VCacheAR/VCacheARSecondViewController.h
+++ VCacheAR/VCacheARSecondViewController.h
@@ -5,9 +5,27 @@
 //  Created by Grif on 2/2/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
-
+#import <CoreLocation/CoreLocation.h>
+#import "SM3DAR.h" 
+#import "AudioToolbox/AudioServices.h"
+#import <MapKit/MapKit.h>
 #import <UIKit/UIKit.h>
 
-@interface VCacheARSecondViewController : UIViewController
+@interface VCacheARSecondViewController : UIViewController <MKMapViewDelegate, SM3DARDelegate, CLLocationManagerDelegate> 
+{
+	SystemSoundID focusSound;
+    BOOL sm3darInitialized;
+    CLLocationManager *locationManager;
+    IBOutlet SM3DARMapView *mapView;
+    
+    
+    CLLocationAccuracy desiredLocationAccuracy;
+    NSInteger desiredLocationAccuracyAttempts;
+    BOOL acceptableLocationAccuracyAchieved;
+}
+
+@property (nonatomic, retain) IBOutlet SM3DARMapView *mapView;
+@property (nonatomic, retain) CLLocationManager *locationManager;
+- (IBAction) refreshButtonTapped;
 
 @end
--- VCacheAR/VCacheARSecondViewController.m
+++ VCacheAR/VCacheARSecondViewController.m
@@ -5,10 +5,47 @@
 //  Created by Grif on 2/2/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
-
+#define IpocQueue dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0) 
+#define IpocURL [NSURL URLWithString: @"http://www.grif.tv/json.php"] 
+#import <MapKit/MapKit.h>
 #import "VCacheARSecondViewController.h"
 
+@interface NSDictionary(JSONCategories)
++(NSDictionary*)dictionaryWithContentsOfJSONURLString:(NSString*)urlAddress;
+-(NSData*)toJSON;
+@end
+
+@implementation NSDictionary(JSONCategories)
++(NSDictionary*)dictionaryWithContentsOfJSONURLString:(NSString*)urlAddress
+{
+    NSData* data = [NSData dataWithContentsOfURL: [NSURL URLWithString: urlAddress] ];
+    __autoreleasing NSError* error = nil;
+    id result = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
+    if (error != nil) return nil;
+    return result;
+}
+
+-(NSData*)toJSON
+{
+    NSError* error = nil;
+    id result = [NSJSONSerialization dataWithJSONObject:self options:kNilOptions error:&error];
+    if (error != nil) return nil;
+    return result;    
+}
+@end
+
 @implementation VCacheARSecondViewController
+@synthesize mapView;
+@synthesize locationManager;
+
+- (void)dealloc 
+{    
+    [mapView release];
+    mapView = nil;
+
+    
+	[super dealloc];
+}
 
 - (void)didReceiveMemoryWarning
 {
@@ -21,6 +58,10 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor blackColor];
+    
+    [self.view setFrame:[UIScreen mainScreen].bounds];
+    [mapView.sm3dar setFrame:self.view.bounds];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -29,6 +70,15 @@
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
+}
+- (IBAction) refreshButtonTapped
+{
+    [self.mapView removeAllAnnotations];
+    dispatch_async(IpocQueue, ^{
+        NSData* data = [NSData dataWithContentsOfURL: IpocURL];
+        [self performSelectorOnMainThread:@selector(fetchedData:) withObject:data waitUntilDone:YES];
+    });
+    
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -39,6 +89,7 @@
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
+    [mapView.sm3dar startCamera];
 }
 
 - (void)viewWillDisappear:(BOOL)animated
