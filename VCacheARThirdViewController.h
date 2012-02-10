b0853a5c144ba12efa3a6a15e1eab70a6d75541d
Grif Priest <Grif@Grifs-Mac-mini.local>
2012-02-10 12:51:17 -0800

    Settings Tab added

---
+++ VCacheARThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  VCacheARThirdViewController.h
+//  VCacheAR
+//
+//  Created by Grif on 2/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCacheARThirdViewController : UIViewController
+
+@end
